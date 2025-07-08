c5172d3ba843bbb6899163222092851cc92fff98
adamalbu <45970222+adamalbu@users.noreply.github.com>
2025-07-08 16:39:10 +0100

    Added ability to create variables

--- yacc.py
+++ yacc.py
@@ -14,25 +14,41 @@ def p_program(p):
 
     global py_ast
     py_ast = ast.Module(
-        body=[ast.Expr(value=cmd, lineno=1, col_offset=0) for cmd in p[0]],
+        body=p[0],
         type_ignores=[]
     )
 
-
-
 def p_print(p):
-    """command : PRINT expression"""
-    p[0] = ast.Call(
-        func=ast.Name(id='print', ctx=ast.Load(), lineno=1, col_offset=0),
-        args=[p[2]],
-        keywords=[],
-        lineno=1,
-        col_offset=0
+    'command : PRINT expression'
+    p[0] = ast.Expr(
+        value=ast.Call(
+            func=ast.Name(id='print', ctx=ast.Load(), lineno=p.lineno(1), col_offset=p.lexpos(1)),
+            args=[p[2]],
+            keywords=[],
+            lineno=p.lineno(1),
+            col_offset=p.lexpos(1)
+        ),
+        lineno=p.lineno(1),
+        col_offset=p.lexpos(1)
     )
 
-def p_string(p):
-    """expression : STRING"""
-    p[0] = ast.Constant(value=p[1][1:-1], lineno=1, col_offset=0)
+def p_assign(p):
+    'command : ID EQUALS expression'
+    p[0] = ast.Assign(
+        targets=[ast.Name(id=p[1], ctx=ast.Store(), lineno=p.lineno(1), col_offset=p.lexpos(1))],
+        value=p[3],
+        lineno=p.lineno(2),
+        col_offset=p.lexpos(2)
+    )
+
+def p_load(p):
+    'expression : ID'
+    p[0] = ast.Name(id=p[1], ctx=ast.Load(), lineno=p.lineno(1), col_offset=p.lexpos(1))
+
+def p_expression_string(p):
+    'expression : STRING'
+    p[0] = ast.Constant(value=p[1][1:-1], lineno=p.lineno(1), col_offset=p.lexpos(1))  # Remove quotes
+
 
 def p_error(p):
     if p:
